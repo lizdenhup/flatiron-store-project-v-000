38023a271d0e952461b62ac50c76521bfc612812
lizdenhup <lizdenhup@gmail.com>
2017-01-03 17:20:06 -0600

    all migrations up

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
-
+    create_table :items do |t|
+      t.string :name
+      t.integer :inventory
+      t.float :price
+      t.references :categories, index: true 
+    end 
   end
 end
--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -1,5 +1,9 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
-
+    create_table :line_items do |t|
+      t.integer :quantity, default: 1
+      t.references :cart, index: true
+      t.references :item, index: true
+    end 
   end
 end
